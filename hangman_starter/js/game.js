05cd8ba82c8cab08b2c92e19fbdf814b012c7b21
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-06-19 13:43:52 -0500

    working on functions

--- hangman_starter/js/game.js
+++ hangman_starter/js/game.js
@@ -1,44 +1,64 @@
-var movieTitle = ["Aladdin", "Gaslight"];
+var movieArray = ["Aladdin", "Gaslight"];
+var movieTitle = "FARGO";
 
 
 //Call on a random movie from array
-function moviePicker() = placeholder;
-
-
-//Collect and check the guessed letter from submission form
-//Loop through movie title, if success call on letterRevealer function
-//if not, reveal elsewhere and call on the strikeCounter function
-function letterChecker() = placeholder;
-
-
-
-//Reveals letter on the line if correct
-function letterRevealer() = placeholder;
-
-	//Called on whenever letterRevealer is sucessfully executed
-	//Check to see if all letters have been revealed
-	//if so, declare winner
-	function winChecker() = placeholder;
+function moviePicker() {
+	var randomNumber = [Math.floor(Math.random()*moviePicker.length)];
+	movieTitle = movieArray[randomNumber];
+	return movieTitle;
+}
 
 
 
+// //Collect and check the guessed letter from submission form
+// //Loop through movie title, if success call on letterRevealer function
+// //if not, reveal elsewhere and call on the strikeCounter function
+// function letterChecker() {
 
-//7 strikes per game, one strike expended per letter.
-//In the case where there are repeat letters...
-//all are revealed, no need for repeat guessing.
-function strikeCounter() = placeholder;
+// }
 
-	//Called on whenever strikeCounter is successfully executed
-	//Check to see how many strikes left out of seven
-	//if >= 7, execute function loserChecker
-	function loserChecker() = placeholder;
+//div generator for movie title
+function wordDisplayer(movieTitle) {
+	$('.game-word').append('<li><div with bottom border><h1>A</h1></div></li>');
+	console.log('herpderp');
+}
 
+// //Reveals letter on the line if correct
+// function letterRevealer(movieTitle) {
 
+// 	//Called on whenever letterRevealer is sucessfully executed
+// 	//Check to see if all letters have been revealed
+// 	//if so, declare winner
+// 	function winChecker() {
+
+// 	}
 
 
 
 
-//Regardless if it is correct or not, 
-//show all letters that have been guessed
-//in the form provided
-function displayLetter() = placeholder;
+// //7 strikes per game, one strike expended per letter.
+// //In the case where there are repeat letters...
+// //all are revealed, no need for repeat guessing.
+// function strikeCounter() {
+
+// }
+
+// 	//Called on whenever strikeCounter is successfully executed
+// 	//Check to see how many strikes left out of seven
+// 	//if >= 7, execute function loserChecker
+// 	function loserChecker() {
+
+// 	}
+
+
+
+
+
+
+// //Regardless if it is correct or not, 
+// //show all letters that have been guessed
+// //in the form provided
+// function displayLetter() {
+
+// }
